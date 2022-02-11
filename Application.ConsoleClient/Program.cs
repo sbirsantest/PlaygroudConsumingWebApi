9bc380468aad4cf0b419a85b6cfbe5efcf656f80
sbarsan <sorinbarsan@gmail.com>
2022-02-11 11:25:12 +0200

    add new project (console application)  to be the client for the WebApi

---
+++ Application.ConsoleClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Application.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
